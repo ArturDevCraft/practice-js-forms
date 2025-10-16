d1ec2cac45dcea2680a65e8d6b55a51b55ec28e6
Artur <artur.czerwiak@gmail.com>
2025-10-16 09:52:30 +0200

    Task 04 completed

--- 04/app.js
+++ 04/app.js
@@ -1,40 +1,59 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const boxElement = document.querySelector('.box');
-    setBoxShadow(boxElement, '#000000');
+	const boxElement = document.querySelector('.box');
+	const panelElement = document.querySelector('.panel');
+	setBoxShadow(boxElement, '#000000');
+	initEvents(boxElement, panelElement);
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
-    const colorInRGBA = `rgba(
+	const colorInRGBA = `rgba(
         ${getChannelColor(colorInHex, 'red')}, 
         ${getChannelColor(colorInHex, 'green')}, 
         ${getChannelColor(colorInHex, 'blue')}, 
         ${opacity}
     )`;
 
-    element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
+	element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
 }
 
+function initEvents(boxElement, panelElement) {
+	const color = panelElement.querySelector('input[name="color"]');
+	const opacity = panelElement.querySelector('input[name="opacity"]');
+
+	color.addEventListener('change', (e) =>
+		onColorChange(color, opacity, boxElement)
+	);
+
+	opacity.addEventListener('mousemove', (e) => {
+		if (e.buttons == 1) {
+			onColorChange(color, opacity, boxElement);
+		}
+	});
+}
+
+function onColorChange(color, opacity, boxElement) {
+	const alpha = opacity.value / 100;
+	setBoxShadow(boxElement, color.value, alpha);
+}
 
 function getChannelColor(colorInHex, channelName) {
-    let start;
-    switch(channelName) {
-        case 'red':
-            start = 1;
-            break;
-        case 'green':
-            start = 3;
-            break;
-        case 'blue':
-            start = 5;
-            break;
-    }
+	let start;
+	switch (channelName) {
+		case 'red':
+			start = 1;
+			break;
+		case 'green':
+			start = 3;
+			break;
+		case 'blue':
+			start = 5;
+			break;
+	}
 
-    const channelColorHex = colorInHex.substr(start, 2);
-    const channelColorDec = parseInt(channelColorHex, 16);
+	const channelColorHex = colorInHex.substr(start, 2);
+	const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+	return channelColorDec;
 }
-
-
